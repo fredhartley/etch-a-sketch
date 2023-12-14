1c0e54bb0b674c6db55cb319f5fd05d358f1fa5a
Fred Hartley <fred@taylorthomas.co.uk>
2023-12-14 07:53:42 +0000

    Finished grid generation JS

--- scripts.js
+++ scripts.js
@@ -1,19 +1,66 @@
-const gridContainer = document.querySelector(".grid-container")
+// global variables for DOM declared here
+const gridContainer = document.getElementById("grid-container")
+const gridSlider = document.getElementById("grid-slider")
+let gridSize = document.getElementById("grid-slider").value
+
+// count the number of grid items
+// if the new value is less, delete them
+// if the new value is more, add them
 
 function generateGrid(gridSize) {
-    for (let i = 0; i < gridSize * gridSize; i++) {
-       // create element
-       const gridElement = document.createElement("div")
-       // assign it a class of grid-item
-       gridElement.classList.add("grid__item")
-       // append it to the grid container
-       gridContainer.append(gridElement)
-} }
+    const gridItems = document.querySelectorAll(".grid__item");
+    const currentGridSize = gridItems.length
+    gridSize = gridSize * gridSize
+
+    if (gridSize > currentGridSize) {
+        const divsToAdd = gridSize - currentGridSize;
+
+        for (let i = 0; i < divsToAdd; i++) {
+            const gridElement = document.createElement("div");
+            gridElement.classList.add("grid__item");
+            gridContainer.append(gridElement);
+        }
+    } else if (gridSize < currentGridSize) {
+        const divsToRemove = currentGridSize - gridSize;
+
+        for (let i = 0; i < divsToRemove; i++) {
+            gridContainer.lastElementChild.remove();
+        }
+    }
+}
+
+function generateGrid2(gridSize) {
+    const gridItems = document.querySelectorAll(".grid__item");
+    const currentGridItems = gridItems.length
+    gridSize = gridSize * gridSize
+
+    if (gridSize > currentGridItems) {
+        const divsToAdd = gridSize - currentGridItems;
+    
+        for (let i = 0; i < divsToAdd; i++) {
+            const gridElement = document.createElement("div");
+            gridElement.classList.add("grid__item");
+            gridContainer.append(gridElement);
+        }
+    }   else if(gridSize < currentGridItems) {
+            const divsToRemove = currentGridItems - gridSize
+        
+            for (let i = 0; i < divsToRemove; i++) {
+                gridContainer.lastElementChild.remove();
+            }
+    }
+    }
 
 // set it so that the grid is 16x16 on load
-// Do I want to do this by fetching the value from the slider?
+// do I want to do this by fetching the value from the slider?
 document.addEventListener("DOMContentLoaded", () => {
+    generateGrid(gridSize)
     // call gridGenerate with a value of 16
 })
 
-// connect to slider
+gridSlider.addEventListener("input", (e) => {
+        gridSize = e.target.value
+        generateGrid(gridSize)
+})
+
+// connect to slider (add event listener to the slider that calls generateGrid)
